e7afd15e74dda1e89b7a0b53dc175f190cf4f16c
chanasorn1234 <69181563+chanasorn1234@users.noreply.github.com>
2020-09-12 13:31:50 +0700

    Update Program5.2.cpp

--- ProgramWeek5/Program5.2.cpp
+++ ProgramWeek5/Program5.2.cpp
@@ -4,25 +4,46 @@
 int main()
 {
 	int x,y=3;
+	char No[] = { 'N','o', ' ', 'C', 'a', 'l', 'c', 'u', 'l', 'a', 't', 'e', };
 	printf("Enter Number:");
 	scanf("%d", &x);
-	printf("2,3,5");
-	for (int i = 6; i <= x; i++)
+	if (x >= 5)
 	{
-		if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0)
-		{
-			printf(",%d", i);
-			y += 1;
-			//Sleep(500);
-		}
+		printf("2,3,5");
 	}
-	printf("\n");
-	printf("last sequence = %d", y);
+	if (x == 4 || x == 3)
+	{
+		printf("2,3");
+		y -= 1;
+	}
+	if (x == 2)
+	{
+		printf("2");
+		y -= 2;
+	}
 
-
-
-
-
-
+	while (true) {
+		if (x <= 1)
+		{
+			for (int j = 0; j <= 11; j++) {
+				printf("%c", No[j]);
+				Sleep(500);				
+			}break;
+		}
+		for (int i = 6; i <= x; i++)
+		{
+			if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0)
+			{
+				printf(",%d", i);
+				y += 1;
+				
+			}
+		}
+		printf("\n");
+		printf("last sequence = %d\n", y);
+		printf("\n\n");
+		main();
+	}
+	
 	return 0;
 }
